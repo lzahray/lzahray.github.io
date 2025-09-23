71883820390abf2ec664dbcd7a98e693397e236a
lzahray <lisazahray@gmail.com>
2025-09-23 04:17:26 -0400

    Deploy built site to main branch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: '/', // Use absolute paths for username.github.io repos
   build: {
     rollupOptions: {
       output: {
